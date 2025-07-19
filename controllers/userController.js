abf760c0b48fa0c9a5f7d8ba7fe257258e24d70a
SerenaSinha31 <Serena31may@gmail.com>
2025-07-19 15:41:06 +0530

    update

--- controllers/userController.js
+++ controllers/userController.js
@@ -23,32 +23,58 @@ catch(err){
     res.status(500).json({message: "Something went wrong"});
 }
 }
-
-const login=async (req,res)=>{
-    try{
-        const {email,password}=req.body;
-        const existingUser = await userModel.findOne({email});
-
-        if(existingUser){
-            const isMatch = await bcrypt.compare(password,existingUser.password);
-
-            if(isMatch){
-                const userObj ={username:existingUser.username,email:existingUser.email,role:existingUser.role};
-                const token = jwt.sign(userObj,SECRET,{expiresIn:"1h"});
-                res.status(200).json({user:userObj,token});
-            }else{
-                res.status(400).json({message: "Invalid Password"});
-            }
-        }else{
-            res.status(400).json({message: "User not found"});
-        }
-
-
-    }catch(err){
-        console.log(err);
-        res.status(500).json({message: "Something went wrong"});
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const existingUser = await userModel.findOne({ email });
+    if (existingUser) {
+      const isMatch = await bcrypt.compare(password, existingUser.password);
+      if (isMatch) {
+        const userObj = {
+          id: existingUser._id,
+          firstName: existingUser.firstName,
+          email: existingUser.email,
+          role: existingUser.role,
+        };
+        const token = jwt.sign(userObj, SECRET, { expiresIn: "1h" });
+        res.status(200).json({ ...userObj, token });
+      } else {
+        res.status(400).json({ message: "Invalid Password" });
+      }
+    } else {
+      res.status(400).json({ message: "User not found" });
     }
-} 
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Something went wrong" });
+  }
+};
+
+// const login=async (req,res)=>{
+//     try{
+//         const {email,password}=req.body;
+//         const existingUser = await userModel.findOne({email});
+
+//         if(existingUser){
+//             const isMatch = await bcrypt.compare(password,existingUser.password);
+
+//             if(isMatch){
+//                 const userObj ={username:existingUser.username,email:existingUser.email,role:existingUser.role};
+//                 const token = jwt.sign(userObj,SECRET,{expiresIn:"1h"});
+//                 res.status(200).json({user:userObj,token});
+//             }else{
+//                 res.status(400).json({message: "Invalid Password"});
+//             }
+//         }else{
+//             res.status(400).json({message: "User not found"});
+//         }
+
+
+//     }catch(err){
+//         console.log(err);
+//         res.status(500).json({message: "Something went wrong"});
+//     }
+// } 
 
 // const showusers=async(req,res)=>{
 //     try{
